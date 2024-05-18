cbf0a86b58cafaf834552f03098deb2c7c3c2005
chrisindris <chrisindris@gmail.com>
2024-05-18 06:09:24 -0400

    Details of run at d_model == 400

--- spot_train_eval.sh
+++ spot_train_eval.sh
@@ -1,3 +1,4 @@
-python spot_train.py
-python spot_inference.py
-python eval.py
+# Usage: ./spot_train_eval.sh <outfile_name.txt>
+{ time python spot_train.py ; } 2>&1 | tee -a outfile.txt
+{ time python spot_inference.py ; } 2>&1 | tee -a outfile.txt
+{ time python eval.py ; } 2>&1 | tee -a outfile.txt
