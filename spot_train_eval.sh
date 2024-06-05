18c21c2ef920900aff0241cee88637b6e95b449b
chrisindris <chrisindris@gmail.com>
2024-06-05 02:58:20 -0400

    organization of output files and the script to generate them

--- spot_train_eval.sh
+++ spot_train_eval.sh
@@ -1,4 +1,7 @@
 # Usage: ./spot_train_eval.sh <outfile_name.txt>
-{ time python spot_train.py ; } 2>&1 | tee -a outfile.txt
-{ time python spot_inference.py ; } 2>&1 | tee -a outfile.txt
-{ time python eval.py ; } 2>&1 | tee -a outfile.txt
+
+echo "$0" "$@" | tee -a "output/experiments/${1}" # record the command used to run this script.
+
+{ time python spot_train.py ; } 2>&1 | tee -a "output/experiments/${1}"
+{ time python spot_inference.py ; } 2>&1 | tee -a "output/experiments/${1}"
+{ time python eval.py ; } 2>&1 | tee -a "output/experiments/${1}"
