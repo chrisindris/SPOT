80f6ccbc9ffd32b391c684cafedab95f2b8d04a0
chrisindris <chrisindris@gmail.com>
2024-06-30 17:32:43 -0400

    further experiments on mostly-labeled data

--- multirun.sh
+++ multirun.sh
@@ -24,36 +24,40 @@ if (( gpu == 0 )); then
 else
   output_pth="./output_2/"
  
-  ./spot_train_eval.sh "$gpu" latest_trial-training_bloss_new-toploss-unlabel_percent_0.4-max_epoch_30.txt ./configs/anet.yaml \
+  ./spot_train_eval.sh "$gpu" latest_trial-training_bloss_new-toploss-unlabel_percent_0.4-max_epoch_20.txt ./configs/anet.yaml \
     dataset.training.unlabel_percent=0.4 \
     dataset.testing.unlabel_percent=0.4 \
-    training.max_epoch=30 \
+    pretraining.warmup_epoch=18 \
+    training.max_epoch=20 \
     dataset.training.output_path=$output_pth \
-    dataset.training.output_path=$output_pth \
+    dataset.testing.output_path=$output_pth \
     training.checkpoint_path=$output_pth
 
-  ./spot_train_eval.sh "$gpu" latest_trial-training_bloss_new-toploss-unlabel_percent_0.3-max_epoch_30.txt ./configs/anet.yaml \
+  ./spot_train_eval.sh "$gpu" latest_trial-training_bloss_new-toploss-unlabel_percent_0.3-max_epoch_20.txt ./configs/anet.yaml \
     dataset.training.unlabel_percent=0.3 \
     dataset.testing.unlabel_percent=0.3 \
-    training.max_epoch=30 \
+    pretraining.warmup_epoch=18 \
+    training.max_epoch=20 \
     dataset.training.output_path=$output_pth \
-    dataset.training.output_path=$output_pth \
+    dataset.testing.output_path=$output_pth \
     training.checkpoint_path=$output_pth
 
-  ./spot_train_eval.sh "$gpu" latest_trial-training_bloss_new-toploss-unlabel_percent_0.2-max_epoch_30.txt ./configs/anet.yaml \
+  ./spot_train_eval.sh "$gpu" latest_trial-training_bloss_new-toploss-unlabel_percent_0.2-max_epoch_20.txt ./configs/anet.yaml \
     dataset.training.unlabel_percent=0.2 \
     dataset.testing.unlabel_percent=0.2 \
-    training.max_epoch=30 \
+    pretraining.warmup_epoch=18 \
+    training.max_epoch=20 \
     dataset.training.output_path=$output_pth \
-    dataset.training.output_path=$output_pth \
+    dataset.testing.output_path=$output_pth \
     training.checkpoint_path=$output_pth
 
-  ./spot_train_eval.sh "$gpu" latest_trial-training_bloss_new-toploss-unlabel_percent_0.1-max_epoch_30.txt ./configs/anet.yaml \
+  ./spot_train_eval.sh "$gpu" latest_trial-training_bloss_new-toploss-unlabel_percent_0.1-max_epoch_20.txt ./configs/anet.yaml \
     dataset.training.unlabel_percent=0.1 \
     dataset.testing.unlabel_percent=0.1 \
-    training.max_epoch=30 \
+    pretraining.warmup_epoch=18 \
+    training.max_epoch=20 \
     dataset.training.output_path=$output_pth \
-    dataset.training.output_path=$output_pth \
+    dataset.testing.output_path=$output_pth \
     training.checkpoint_path=$output_pth
 
 fi
